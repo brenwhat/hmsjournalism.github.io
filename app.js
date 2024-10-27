f29f8d1168eb0cf40dc08f1f1ebc1ccc418ade86
brenwhat <brennanwong64@gmail.com>
2024-10-26 22:21:31 -0700

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const artGallery = {
     1: { title: 'wow im a cool title', author: 'Skibidi Toilet', grade: "6", caption : "hey this is a demo caption just to see how itd look la tee ta tee ta tis i ajklsdfjlkasdljkfajklsdfjlkasdljkfajklsdfjlkasdljkfajklsdfjlkasdljkfajklsdfjlkasdljkf", imageURL: "./workTypes/images/BingoPFP.jpg", coverImage: "./workTypes/images/BingoPFP.jpg"},
     2: { title: 'very unique title', author: 'Name One & Name Two', grade: "7 & 8", caption : "hey this is a demo caption just to see how itd look", imageURL: "./workTypes/images/dababy.jpg", coverImage: "./workTypes/images/dababy.jpg"},
     3: { title: 'nice art', author: 'I have an uncessarily long name', grade: "8", caption : "hey this is a demo caption just to see how itd look", imageURL: "./workTypes/images/fatdog.jpg", coverImage: "./workTypes/images/fatdog.jpg"},
-    4: { title: 'egwin my beloved', author: 'Daghan', grade: "6", caption : "this is my egwin hes very cool and this caption is also very cool. also hes a test for cover images being different", imageURL: "./workTypes/images/egwin.jpg", coverImage: "./workTypes/images/squaresquirrel.jpg"}
+    4: { title: 'egwin my beloved', author: 'Daghan', grade: "6", caption : "this is my egwin hes very cool and this caption is also very cool. also hes a test for cover images being different", imageURL: "./workTypes/images/egwin.jpg", coverImage: "./workTypes/images/squaresquirrel.jpg"},
+    5: { title: 'egwin my beloved', author: 'Daghan', grade: "6", caption : "this is my egwin hes very cool and this caption is also very cool. also hes a test for cover images being different", imageURL: "./workTypes/images/hunterwallpaper2.png", coverImage: "./workTypes/images/RYANGOSLING.jpg"}
 };
 
 const textGallery = {
@@ -20,23 +21,27 @@ const Galleries = [
   hybridGallery,
 ];
 
+function galleryRefresh() {
+  document.getElementById("gimage").src = localStorage.getItem("image") + '#toolbar=0';
+  document.getElementById("gtitle").innerHTML = localStorage.getItem("title");
+  document.getElementById("gauthor").innerHTML = localStorage.getItem("author") + ', Grade ' + localStorage.getItem("grade");
+  document.getElementById("gcaption").innerHTML = localStorage.getItem("caption");
+}
+
 function loadPage(type, id) {
   const targetWindow = window.open('gallery.html');
 
-  targetWindow.onload = function() {
-
-  targetWindow.document.getElementById("gimage").src = (Galleries[type])[id].imageURL + '#toolbar=0';
-  targetWindow.document.getElementById("gtitle").innerHTML = (Galleries[type])[id].title;
-  targetWindow.document.getElementById("gauthor").innerHTML = (Galleries[type])[id].author + ', Grade ' + (Galleries[type])[id].grade;
-  targetWindow.document.getElementById("gcaption").innerHTML = (Galleries[type])[id].caption;
-  };
+  localStorage.setItem("image", (Galleries[type])[id].imageURL);
+  localStorage.setItem("title", (Galleries[type])[id].title);
+  localStorage.setItem("author", (Galleries[type])[id].author);
+  localStorage.setItem("grade", (Galleries[type])[id].grade);
+  localStorage.setItem("caption", (Galleries[type])[id].caption);
 };
 
 function fillArt(type) {
   const length = Object.keys(Galleries[type]).length;
 
   for (let i=1; i<length+1; i++) {
-    console.log((Galleries[type])[i]);
 
     document.getElementById("gallerygrid").innerHTML += `<div class="artwhole" onclick="loadPage(${type + ", " + i});"> \
                 <img class="Art" src="${(Galleries[type])[i].coverImage}">    \
